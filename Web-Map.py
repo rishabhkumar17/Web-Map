c96a3e04e902482cb94a952236d04f76016c2f0b
Rishabh <rishabhkaushal1796@gmail.com>
2022-05-04 22:10:54 +0530

    Circle Marker

--- Web-Map.py
+++ Web-Map.py
@@ -18,7 +18,7 @@ map = folium.Map(location=[28.65, 77.25], zoom_start=6, tiles = "Stamen Terrain"
 fg = folium.FeatureGroup(name="My Map")
 #add markers
 for lt, ln, name, cp in zip(lat,lon, nam, cap):
-    fg.add_child(folium.Marker(location=[lt, ln], popup=name, icon=folium.Icon(color=color_producer(cp))))
+    fg.add_child(folium.CircleMarker(location=[lt, ln], radius=5, popup=name, fill_color=color_producer(cp), color='grey', fill_opacity=0.7))
 
 map.add_child(fg)
 
