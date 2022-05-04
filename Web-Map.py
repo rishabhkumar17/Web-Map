84cb1995ff03fd5df43c71ba2b6ef61e53b3f723
Rishabh <rishabhkaushal1796@gmail.com>
2022-05-04 21:36:07 +0530

    Adding multiple markers using coordinates file

--- Web-Map.py
+++ Web-Map.py
@@ -1,8 +1,17 @@
 import folium
+import pandas as pd
+
+data = pd.read_csv("in.csv")
+lat = list(data["lat"])
+lon = list(data["lng"])
 
 map = folium.Map(location=[28.65, 77.25], zoom_start=6, tiles = "Stamen Terrain")
 
-#add market
-map.add_child(folium.Marker(location=[28.65, 77.25], popup="Hi I am a Marker", icon=folium.Icon(color='green')))
+fg = folium.FeatureGroup(name="My Map")
+#add markers
+for lt, ln in zip(lat,lon):
+    map.add_child(folium.Marker(location=[lt, ln], popup="Hi I am a Marker", icon=folium.Icon(color='green')))
+
+map.add_child(fg)
 
 map.save("Map1.html")
