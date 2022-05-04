1d0ba4ebe496d2fcfbb050a5bf5ce0c9bee2c539
Rishabh <rishabhkaushal1796@gmail.com>
2022-05-04 21:49:07 +0530

    Add popup windows

--- Web-Map.py
+++ Web-Map.py
@@ -4,13 +4,14 @@ import pandas as pd
 data = pd.read_csv("in.csv")
 lat = list(data["lat"])
 lon = list(data["lng"])
+nam = list(data["city"])
 
 map = folium.Map(location=[28.65, 77.25], zoom_start=6, tiles = "Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
 #add markers
-for lt, ln in zip(lat,lon):
-    map.add_child(folium.Marker(location=[lt, ln], popup="Hi I am a Marker", icon=folium.Icon(color='green')))
+for lt, ln, name in zip(lat,lon, nam):
+    fg.add_child(folium.Marker(location=[lt, ln], popup=name, icon=folium.Icon(color='green')))
 
 map.add_child(fg)
 
