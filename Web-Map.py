953e1436303c5b7f394988c8df18da0a8c217b04
Rishabh <rishabhkaushal1796@gmail.com>
2022-05-04 21:01:01 +0530

    add marker

--- Web-Map.py
+++ Web-Map.py
@@ -1,4 +1,8 @@
 import folium
 
-map = folium.Map(location=[28.65, 77.25])
+map = folium.Map(location=[28.65, 77.25], zoom_start=6, tiles = "Stamen Terrain")
+
+#add market
+map.add_child(folium.Marker(location=[28.65, 77.25], popup="Hi I am a Marker", icon=folium.Icon(color='green')))
+
 map.save("Map1.html")
